34a9fe22240c23865f076ac1ee1c6b5715e45fa5
j10c <blyb1739@gmail.com>
2023-07-06 19:53:52 +0800

    feat(error): listen error event and handler

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,16 +4,35 @@ import { Header, Map, PackageList, CarModel } from "@/components";
 import { Socket, io } from "socket.io-client";
 import { useEffect, useState } from "react";
 import SocketContext from "@/hooks/useSocketContext"
+import { notification } from "antd";
 
 export default function Page() {
   const [ws, setWS] = useState<Socket>()
+  const [notice, contextHolder] = notification.useNotification();
 
   useEffect(() => {
-    setWS(io("http://127.0.0.1:5000", { reconnectionAttempts: 10 }));
+    setWS(io("http://192.168.11.69:5000", { reconnectionAttempts: 10 }));
   }, [])
+
+  const openNotification = (message: string) => {
+    notice.error({
+      message: "故障",
+      description: message,
+      duration: 0,
+      placement: "bottomLeft"
+    })
+  }
+
+  useEffect(() => {
+    if (!ws) return;
+    ws.on("handleError", (error) => {
+      openNotification(error);
+    })
+  }, [ws])
 
   return (
     <SocketContext.Provider value={{ ws, setWS }}>
+      { contextHolder }
       <Header />
       <main>
         <section style={{
